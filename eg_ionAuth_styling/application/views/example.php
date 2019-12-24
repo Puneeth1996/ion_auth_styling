244198516c982b8200ca203f2dbc9d39d657db39
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-12-24 13:35:33 +0530

    Making settlements with Header And Some Css Issues

--- eg_ionAuth_styling/application/views/example.php
+++ eg_ionAuth_styling/application/views/example.php
@@ -9,7 +9,7 @@ foreach($css_files as $file): ?>
 <?php endforeach; ?>
 </head>
 <body>
-	<div>
+	<!-- <div>
 		<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
 		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
 		<a href='<?php echo site_url('examples/products_management')?>'>Products</a> |
@@ -18,9 +18,9 @@ foreach($css_files as $file): ?>
 		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> |
 		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a>
 		
-	</div>
-	<div style='height:20px;'></div>  
-    <div style="padding: 10px">
+	</div> -->
+	<!-- <div style='height:20px;'></div> -->
+    <div style="padding: 10px;margin-top:50px;">
 		<?php echo $output; ?>
     </div>
     <?php foreach($js_files as $file): ?>
