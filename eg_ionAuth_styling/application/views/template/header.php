be75d48cbe285a730b8839df51eef58e1bd7fdce
Puneeth1996 <puneeth1996p@gmail.com>
2019-12-30 11:44:49 +0530

    Working on Modifying the Grocery Curd Tables

--- eg_ionAuth_styling/application/views/template/header.php
+++ eg_ionAuth_styling/application/views/template/header.php
@@ -49,8 +49,8 @@
             <div class="menu-sidebar2__content js-scrollbar1">
                 <nav class="navbar-sidebar2">
                     <ul class="list-unstyled navbar__list">
-                        <li>Mixed Grid</li>
-                        <li>Blog Page</li>
+                        <li><a href='./Home/demo'> Demo Grid </a></li>
+                        <li><a href='./Home/blog_admin'> employees Grid </a></li>
                         <li><a class="sign_out_Header" href="<?php echo base_url(); ?>index.php/auth/logout">Sign out</a></li>
                     </ul>
                 </nav>
@@ -88,8 +88,8 @@
                 <div class="menu-sidebar2__content js-scrollbar2">
                     <nav class="navbar-sidebar2">
                     <ul class="list-unstyled navbar__list">
-                        <li>Mixed Grid</li>
-                        <li>Blog Page</li>
+                        <li><a href='./Home/demo'> Demo Grid </a></li>
+                        <li><a href='./Home/blog_admin'> employees Grid </a></li>
                         <li><a class="sign_out_Header" href="<?php echo base_url(); ?>index.php/auth/logout">Sign out</a></li>
                     </ul>
                     </nav>
